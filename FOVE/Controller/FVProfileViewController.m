583244494b161757d596a7cf63ba944d37bd05a5
Nutchaphon Rewik <nRewik@Nutchaphons-MacBook-Air.local>
2014-02-28 01:13:32 +0700

    mockup editing profile

--- FOVE/Controller/FVProfileViewController.m
+++ FOVE/Controller/FVProfileViewController.m
@@ -7,23 +7,29 @@
 //
 
 #import "FVProfileViewController.h"
-#import <FacebookSDK/FacebookSDK.h>
-#import "FVAppDelegate.h"
 #import "FVUser.h"
+#import "FVProfileTableViewCell.h"
 
-@interface FVProfileViewController ()
+@interface FVProfileViewController () <UITableViewDataSource,UITableViewDelegate,UIActionSheetDelegate,UIAlertViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UINavigationBar *navigationBar;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *editProfileButton;
+@property (strong,nonatomic) UIBarButtonItem *doneEditProfileButton;
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *backButton;
 @property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *statusLabel;
 @property (weak, nonatomic) IBOutlet UILabel *aboutMeLabel;
-@property (weak, nonatomic) IBOutlet UILabel *genderLabel;
-@property (weak, nonatomic) IBOutlet UILabel *ageLabel;
-@property (weak, nonatomic) IBOutlet UILabel *relationshipLabel;
-@property (weak, nonatomic) IBOutlet UILabel *interestedGenderLabel;
-@property (weak, nonatomic) IBOutlet UILabel *hobbyLabel;
 
+@property (weak, nonatomic) IBOutlet UITableView *detailTableView;
+
+@property (strong,nonatomic) UIActionSheet *sexActionSheet;
+@property (strong,nonatomic) UIActionSheet *relationShipActionSheet;
+
+@property (strong,nonatomic) UIAlertView *statusAlertView;
+
+@property (strong,nonatomic) NSMutableArray *details; // or NSString;
 @property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *photos;
 
 
@@ -31,47 +37,220 @@
 
 @implementation FVProfileViewController
 
--(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    return [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-}
 
 - (void)viewDidLoad
 {
     NSAssert( self.user != nil , @"self.user should not be nil !!");
-    
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    UIGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(editStatus)];
+    [self.statusLabel addGestureRecognizer:tapGesture];
+    self.statusLabel.userInteractionEnabled = NO;
+    
+    self.detailTableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
     [self updateUI];
 }
+-(void)viewWillAppear:(BOOL)animated
+{
+    [self.detailTableView reloadData];
+}
+
+#pragma mark - lazy instantiation
+-(NSMutableArray *)details
+{
+    if (!_details) {
+        _details = [[NSMutableArray alloc] init];
+        
+        [_details addObject:self.user.gender]; //gender
+        [_details addObject:[NSString stringWithFormat:@"%d",self.user.age]]; //age
+        [_details addObject:self.user.relationship];
+//        [_details addObject:self.user.interestedGender];
+//        [_details addObject:self.user.hobby];
+        
+    }
+    return _details;
+}
+-(UIBarButtonItem *)doneEditProfileButton
+{
+    if (!_doneEditProfileButton) {
+        _doneEditProfileButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                                                                               target:self
+                                                                               action:@selector(finishEditing)];
+    }
+    return _doneEditProfileButton;
+}
+-(UIActionSheet *)sexActionSheet
+{
+    if (!_sexActionSheet) {
+        _sexActionSheet = [[UIActionSheet alloc] initWithTitle:@"Gender"
+                                                      delegate:self
+                                             cancelButtonTitle:@"Cancel"
+                                        destructiveButtonTitle:nil
+                                             otherButtonTitles:@"Male",@"Female",@"Other",nil];
+    }
+    return _sexActionSheet;
+}
+-(UIActionSheet *)relationShipActionSheet
+{
+    if (!_relationShipActionSheet) {
+        _relationShipActionSheet = [[UIActionSheet alloc] initWithTitle:@"Relationship"
+                                                               delegate:self
+                                                      cancelButtonTitle:@"Cancel"
+                                                 destructiveButtonTitle:nil
+                                                      otherButtonTitles:@"Single",@"Married",@"Complicate",nil];
+    }
+    return _relationShipActionSheet;
+}
+-(UIAlertView *)statusAlertView
+{
+    if (!_statusAlertView) {
+        _statusAlertView = [[UIAlertView alloc] initWithTitle:@"Edit Status"
+                                                      message:nil
+                                                     delegate:self
+                                            cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok", nil];
+        _statusAlertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+    }
+    return _statusAlertView;
+}
+
+#pragma mark - action
 - (IBAction)goBack:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
-
--(void)updateUI
+- (IBAction)editProfile:(id)sender
 {
-    if( self.user != nil)
-    {
-        self.nameLabel.text = self.user.name;
-        self.statusLabel.text = self.user.status;
-        
-//        self.aboutMeLabel.text = self.user.aboutMe;
-        [self.aboutMeLabel sizeToFit];
-        
-        self.genderLabel.text = self.user.gender;
-        self.ageLabel.text = [NSString stringWithFormat:@"%d",self.user.age];
-        self.relationshipLabel.text = self.user.relationship;
-//        self.interestedGenderLabel.text = self.user.interestedGender;
-//        self.hobbyLabel.text = self.user.hobby;
-        
-        self.profileImageView.image = self.user.profileImage;
-        
-        for (UIImageView *imv in self.photos) {
-            imv.image = self.user.profileImage;
-        }
-        
+    [self setRightBarButtonItem:self.doneEditProfileButton];
+    [self.detailTableView setEditing:YES animated:YES];
+    
+    if (self.statusLabel.text == nil) {
+        self.statusLabel.text = @"Edit Status Here...";
+        [self.statusLabel sizeToFit];
     }
+    self.statusLabel.textColor = [UIColor blueColor];
+    self.statusLabel.userInteractionEnabled = YES;
+}
+-(void)finishEditing
+{
+    [self setRightBarButtonItem:self.editProfileButton];
+    [self.detailTableView setEditing:NO animated:YES];
+    
+    self.statusLabel.textColor = [UIColor blackColor];
+    self.statusLabel.userInteractionEnabled = NO;
+}
+-(void)editStatus
+{
+    [self.statusAlertView show];
 }
 
+#pragma mark - UI
+-(void)updateUI
+{
+    self.details = nil;
+    
+    self.nameLabel.text = self.user.name;
+    self.statusLabel.text = self.user.status;
+    
+//    self.aboutMeLabel.text = self.user.aboutMe;
+    
+    [self.aboutMeLabel sizeToFit];
+    
+    self.profileImageView.image = self.user.profileImage;
+    
+    for (UIImageView *imv in self.photos) {
+        imv.image = self.user.profileImage;
+    }
+    
+    if (self.user != [FVUser currentUser]) {
+        [self setRightBarButtonItem:nil];
+    }
+    
+    [self.detailTableView reloadData];
+}
+-(void)setRightBarButtonItem:(UIBarButtonItem *)button
+{
+    button.tintColor = [UIColor whiteColor];
+    
+    UINavigationItem *item =  self.navigationBar.items[0];
+    item.rightBarButtonItem = button;
+}
+#pragma mark - UITableViewDataSource
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.details count];
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *profileCellIdentifier = @"profileCell";
+    FVProfileTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:profileCellIdentifier forIndexPath:indexPath];
+    cell.textLabel.text = self.details[indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return UITableViewCellEditingStyleNone;
+}
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSUInteger row = indexPath.row;
+    if (row == 0) {
+        [self.sexActionSheet showInView:self.view];
+    }
+    else if(row == 2)
+    {
+        [self.relationShipActionSheet showInView:self.view];
+    }
+
+}
+
+#pragma mark - UIActionSheetDelegate
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (actionSheet == self.sexActionSheet && buttonIndex != self.sexActionSheet.numberOfButtons-1) {
+        self.user.gender = [self.sexActionSheet buttonTitleAtIndex:buttonIndex];
+        
+        NSIndexPath *path = [NSIndexPath indexPathForRow:0 inSection:0];
+        [self.detailTableView deselectRowAtIndexPath:path animated:YES];
+        
+        [self updateUI];
+    }
+    else if(actionSheet == self.relationShipActionSheet && buttonIndex != self.relationShipActionSheet.numberOfButtons-1) {
+        self.user.relationship = [self.relationShipActionSheet buttonTitleAtIndex:buttonIndex];
+        
+        NSIndexPath *path = [NSIndexPath indexPathForRow:2 inSection:0];
+        [self.detailTableView deselectRowAtIndexPath:path animated:YES];
+        
+        [self updateUI];
+    }
+}
+#pragma mark - UIAlertViewDelegate
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (alertView == self.statusAlertView && buttonIndex != 0) {
+        
+        NSString *text = [[self.statusAlertView textFieldAtIndex:0] text];
+        text = [text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        if ( ! [text isEqualToString:@""] ) {
+            self.user.status = text;
+            [self updateUI];
+        }
+    }
+}
 @end
+
+
+
+
+
+
+
+
---
+++ FOVE/View/FVProfileTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FVProfileTableViewCell.h
+//  FOVE
+//
+//  Created by Nutchaphon Rewik on 2/27/14.
+//  Copyright (c) 2014 Nutchaphon Rewik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FVProfileTableViewCell : UITableViewCell
+
+@end
